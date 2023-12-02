8ed0cce09d8e51c5e5a5f3ddcab833aa0e3eebfd
Torben <torbensky@users.noreply.github.com>
2023-12-02 09:09:35 -0800

    day 2, part 1, python

---
+++ day02/solution.py
@@ -0,0 +1,41 @@
+import re, sys
+from functools import reduce
+
+linere = re.compile(r'Game (\d+): (.*)')
+roundre = re.compile(r'(\d+) (red|blue|green)')
+
+class Game:
+    def __init__(self, line):
+        matched = linere.match(line).groups()
+        self.gameid = int(matched[0])
+        rounds = matched[1].split(';')
+        self.rounds = [parseRound(r) for r in rounds]
+    
+    def max(self, color: str) -> int:
+        return reduce(lambda acc, cur: cur[color] if cur.get(color,0) > acc else acc,self.rounds, 0)
+
+def parseLine(line: str) -> Game:
+    return Game(line)
+
+def parseRound(round: str) -> dict[str,int]:
+    d = {}
+    for r in roundre.findall(round):
+        d[r[1]] = int(r[0])
+    return d
+
+
+def part1(lines: list[str]):
+    maxr,maxg,maxb = 12,13,14
+    result = 0
+    for line in lines:
+        g = parseLine(line)
+        if g.max('red') > maxr or g.max('blue') > maxb or g.max('green') > maxg:
+            continue
+        result += g.gameid
+        
+    print('part 1:', result)
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as file:
+        lines = [line.rstrip() for line in file]  
+    part1(lines)
