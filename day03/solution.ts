0602df1bfd11172ad30550b4488d1ece6ee4b8e9
Torben <torbensky@users.noreply.github.com>
2023-12-03 10:41:36 -0800

    day 3, part 1, typescript

---
+++ day03/solution.ts
@@ -0,0 +1,73 @@
+import { readFileSync } from "fs";
+
+interface Coord {
+    x: number
+    y: number
+}
+interface GridNode<T>{
+    value: T
+    pos: Coord
+}
+class Grid<T> {
+    data = new Map<string,GridNode<T>>()
+    add(pos: Coord, val: T){
+        this.data.set(`${pos.x},${pos.y}`, {value: val, pos})
+    }
+    get(pos:Coord){
+        this.data.get(`${pos.x},${pos.y}`)
+    }
+    forEach(cb: (v: GridNode<T>, k: string) => void){
+        this.data.forEach(cb)
+    }
+}
+
+const boundary = (c: Coord, width: number) => ({min: {x: c.x-1, y:c.y-1}, max: {x: c.x + width, y: c.y+1}})
+const within = (c: Coord, bounds: {min: Coord, max: Coord}) => c.x >= bounds.min.x && c.y >= bounds.min.y && c.x <= bounds.max.x && c.y <= bounds.max.y
+
+const inRange = (n: number, min: number, max: number) => n >= min && n <= max
+const isDigit = (ch: string) => inRange(ch.charCodeAt(0) - '0'.charCodeAt(0), 0, 9)
+
+function parseGrid(lines: string[]){
+    const nums = new Grid<number>()
+    const symbols = new Grid<string>()
+    lines.forEach((line, y) => {
+        let numStr = '', numX = -1
+        for(let x = 0; x < line.length; x++){
+            const ch = line.charAt(x)
+            if(ch === '.'){
+                // empty
+            }else if(isDigit(ch)){
+                numStr += ch
+                numX = numX === -1 ? x : numX
+                if(x < line.length - 1){
+                    continue
+                }
+            }else{
+                symbols.add({x,y}, ch)
+            }
+            
+            if(numStr.length > 0){
+                nums.add({x: numX,y}, parseInt(numStr))
+                numStr = ''
+                numX = -1
+            }
+        }
+    })
+    return {nums, symbols}
+}
+
+function part1(lines: string[]){
+    let result = 0
+    const {nums, symbols} = parseGrid(lines)
+    nums.forEach((n) => {
+        symbols.forEach(s => {
+            if(within(s.pos, boundary(n.pos, n.value.toString().length))){
+                result += n.value
+            }
+        })
+    })
+    console.log(`Part 1: ${result}`)
+}
+
+const lines = readFileSync(process.argv[2]).toString().trim().split('\n')
+part1(lines)
