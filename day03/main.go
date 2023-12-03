a9d61ec916fca29a97e2008896b350e5098c46a3
Torben <torbensky@users.noreply.github.com>
2023-12-03 09:56:06 -0800

    day 3, part 1, go

---
+++ day03/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type coord struct {
+	x int
+	y int
+}
+type gridNumBuilder struct {
+	sb  strings.Builder
+	pos coord
+}
+
+func (gnb *gridNumBuilder) setPos(c coord) {
+	gnb.pos = c
+}
+
+func (gnb *gridNumBuilder) addDigit(c rune) {
+	gnb.sb.WriteRune(c)
+}
+
+func (gnb gridNumBuilder) empty() bool {
+	return gnb.sb.Len() == 0
+}
+
+func (gnb *gridNumBuilder) flush() (coord, gridNum) {
+	pos := gnb.pos
+	gn := parseNum(gnb.sb.String())
+	gnb.reset()
+	return pos, gn
+}
+
+func (gnb *gridNumBuilder) reset() {
+	gnb.pos = coord{}
+	gnb.sb.Reset()
+}
+
+type gridNum int
+
+func (g gridNum) bounds(pos coord) (coord, coord) {
+	l := len(strconv.Itoa(int(g)))
+	return coord{x: pos.x - 1, y: pos.y - 1}, coord{x: pos.x + l, y: pos.y + 1}
+}
+
+type numGrid map[coord]gridNum
+type symbolGrid map[coord]rune
+
+func contains(p coord, min coord, max coord) bool {
+	return p.x >= min.x && p.y >= min.y && p.x <= max.x && p.y <= max.y
+}
+
+func parseNum(s string) gridNum {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return gridNum(n)
+}
+
+func parseGrid(lines []string) (numGrid, symbolGrid) {
+	nums := numGrid{}
+	symbols := symbolGrid{}
+	for y, line := range lines {
+		var gnb gridNumBuilder
+		for x, ch := range line {
+			c := coord{x: x, y: y}
+			switch ch {
+			case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+				// handle digit
+				if gnb.empty() {
+					gnb.setPos(c)
+				}
+				gnb.addDigit(ch)
+				// process more characters as long as we're not at the EOL
+				if x < len(line)-1 {
+					continue
+				}
+			case '.':
+				// empty, do nothing
+			default:
+				// handle symbol
+				symbols[c] = ch
+			}
+
+			if !gnb.empty() {
+				pos, gn := gnb.flush()
+				nums[pos] = gn
+			}
+		}
+	}
+	return nums, symbols
+}
+
+func part1(lines []string) {
+	result := 0
+	nums, symbols := parseGrid(lines)
+	for npos, num := range nums {
+		for spos := range symbols {
+			min, max := num.bounds(npos)
+			if contains(spos, min, max) {
+				result += int(num)
+			}
+		}
+	}
+	fmt.Printf("Part 1: %d\n", result)
+}
+
+func main() {
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+	part1(lines)
+}
