5140665696c2b73c02c6bcc439f65dd3fceef524
Torben <torbensky@users.noreply.github.com>
2023-12-06 01:00:42 +0000

    day 5, part 2, python

--- day05/solution.py
+++ day05/solution.py
@@ -12,6 +12,18 @@ class Mapping:
             return self.dest + diff
         return None
 
+    def map_range(self, source: tuple[int,int]):
+        before,mapped,after = None,None,None
+        start, end = self.source, self.source + self.length - 1
+        if start > source[0]:
+            before = (source[0], min(source[1], start -1))
+        if start <= source[1] and end >= source[0]:
+            mapped = (self.map(max(source[0], start)), self.map(min(source[1], end)))
+        if end < source[1]:
+            after = (max(source[0], end + 1), source[1])
+        return (before, mapped, after)
+
+
 class MappingList:
     def __init__(self, mappings: list[Mapping]):
         self.mappings = mappings
@@ -22,6 +34,24 @@ class MappingList:
             if v is not None:
                 return v        
         return source
+
+    def map_range(self, source: tuple[int,int]) -> list[tuple[int,int]]:
+        result = []
+        toMap = [source]
+        for m in self.mappings:
+            next = []
+            for tm in toMap:
+                before, mapped, after = m.map_range(tm)
+                if before:
+                    next.append(before)
+                if mapped:
+                    result.append(mapped)
+                if after:
+                    next.append(after)
+            toMap = next
+        result.extend(toMap) # include any last ranges that didn't get mapped
+        return result
+        
 
 def parse_seeds(line: str) -> list[int]:
     return [int(s) for s in line.split()[1:]]
@@ -49,9 +79,26 @@ def part1(parts: list[str]):
     
     print('Part 1:', min(locations))
                 
-        
+def part2(parts: list[str]):
+    seedPairs, mappings = parse_input(parts)
+    locations = []
+    # seeds are ranges, instead of individual seeds now
+    seedPairs = zip(seedPairs[::2],seedPairs[1::2])
+    for pair in seedPairs:
+        seedRanges = [(pair[0], pair[0] + pair[1] - 1)]
+        for m in mappings:
+            next = []
+            for sr in seedRanges:
+                # when a range is mapped, you can get a list of ranges back
+                next += m.map_range(sr)
+            seedRanges = next
+            next = []
+        locations.append(min(seedRanges)[0]) # take the beginning of min range as the lowest location
+    
+    print('Part 2:', min(locations))     
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as file:
         parts = file.read().split('\n\n')
     part1(parts)
+    part2(parts)
