527d99b60d14fa316ead5e872b06f9fd6ac6a7d8
Torben <torbensky@users.noreply.github.com>
2023-12-13 15:32:30 -0800

    day 13, part 2, go

--- day13/main.go
+++ day13/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"log/slog"
 	"os"
-	"slices"
 	"strings"
 	"time"
 )
@@ -75,66 +74,93 @@ func parseInput() (result []grid) {
 	return result
 }
 
-func (g grid) isColReflectionPoint(idx int) bool {
+func diff(a, b []bool) (diffs int) {
+	// NOTE: assuming a and b are the same length
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			diffs++
+		}
+	}
+	return diffs
+}
+
+func (g grid) isColReflectionPoint(idx int, smudge bool) bool {
+	smudgeAmount := 0
+	if smudge {
+		smudgeAmount = 1
+	}
+	numDifferences := 0
 	for i, j := idx, idx+1; i >= 0 && j < g.width(); i, j = i-1, j+1 {
-		if !slices.Equal(g.col(i), g.col(j)) {
+		numDifferences += diff(g.col(i), g.col(j))
+		if numDifferences > smudgeAmount {
 			return false
 		}
 	}
-	return true
+	return numDifferences == smudgeAmount
 }
 
-func (g grid) isRowReflectionPoint(idx int) bool {
+func (g grid) isRowReflectionPoint(idx int, smudge bool) bool {
+	smudgeAmount := 0
+	if smudge {
+		smudgeAmount = 1
+	}
+	numDifferences := 0
 	for i, j := idx, idx+1; i >= 0 && j < g.height(); i, j = i-1, j+1 {
-		if !slices.Equal(g.row(i), g.row(j)) {
+		numDifferences += diff(g.row(i), g.row(j))
+		if numDifferences > smudgeAmount {
 			return false
 		}
 	}
-	return true
+	return numDifferences == smudgeAmount
 }
 
-func (g grid) findReflection() (row int, col int) {
-	row, col = -1, -1
+func (g grid) findReflection(smudge bool) (row int, col int) {
 	for i := 0; i < g.width()-1; i++ {
-		if g.isColReflectionPoint(i) {
-			if col >= 0 {
-				panic("multiple")
-			}
-			col = i
+		if g.isColReflectionPoint(i, smudge) {
+			return -1, i
 		}
 	}
 	for i := 0; i < g.height()-1; i++ {
-		if g.isRowReflectionPoint(i) {
-			if row >= 0 {
-				panic("multiple")
-			}
-			row = i
+		if g.isRowReflectionPoint(i, smudge) {
+			return i, -1
 		}
 	}
-	return row, col
+	panic("No reflection found when one was expected")
 }
 
 func part1(patterns []grid) {
 	answer := 0
 	for _, p := range patterns {
-		row, col := p.findReflection()
+		row, col := p.findReflection(false)
 		if col >= 0 {
 			answer += col + 1
 		}
 		if row >= 0 {
 			answer += 100 * (row + 1)
 		}
-		// fmt.Println(p)
-		fmt.Println(row, col, " => ", answer)
-		// fmt.Scanln()
 	}
 	fmt.Printf("Part 1: %d\n", answer)
+}
+
+func part2(patterns []grid) {
+	answer := 0
+	for _, p := range patterns {
+		row, col := p.findReflection(true)
+		if col >= 0 {
+			answer += col + 1
+		}
+		if row >= 0 {
+			answer += 100 * (row + 1)
+		}
+	}
+	fmt.Printf("Part 2: %d\n", answer)
 }
 
 func main() {
 	start := time.Now()
 	patterns := parseInput()
 	part1(patterns)
+	part2(patterns)
 	elapsed := time.Since(start)
 	log.Printf("Took %s", elapsed)
 }
