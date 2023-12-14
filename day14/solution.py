02096a3cf3ff287647cf7dd432c17c5e6c3a6fdf
Torben <torbensky@users.noreply.github.com>
2023-12-14 11:54:20 -0800

    day 14, part 2, python

--- day14/solution.py
+++ day14/solution.py
@@ -19,12 +19,39 @@ def fall(grid):
             if c == '#':
                 anchor = row + 1
 
+def cycle(grid):
+    for i in range(4):
+        fall(grid)
+        grid = [list(e) for e in zip(*grid[::-1])]
+    return grid
+
 def part1(grid):
     fall(grid)
     print(score(grid))
-   
+
+def part2(grid):
+    cache = dict()
+    i = 0
+    target = 1000000000
+    skipped = False
+    while i < target:
+        grid = cycle(grid)
+        i += 1
+        key = "".join(["".join(r) for r in grid])
+        if not skipped:
+            if key in cache:
+                # We saw this before! Cycle detected!
+                period = i - cache[key]
+                # FAST FORWAAAARD!
+                i = target - ((target - i) % period)
+                skipped = True
+            else:
+                cache[key] = i
+    print(f'Part 2: {score(grid)}')
+    
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         data = f.read()
     grid = [list(line) for line in data.split('\n')]
     part1([g.copy() for g in grid])
+    part2(grid)
